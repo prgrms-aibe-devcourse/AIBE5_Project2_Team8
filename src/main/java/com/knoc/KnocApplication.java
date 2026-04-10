b4dfc64fbd6844c8e83f98e45d6116752d6a942c
jhwna1205-sys <jhwan1205@gmail.com>
2026-04-11 03:07:01 +0900

    first commit

---
+++ src/main/java/com/knoc/KnocApplication.java
@@ -0,0 +1,13 @@
+package com.knoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knoc/KnocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
