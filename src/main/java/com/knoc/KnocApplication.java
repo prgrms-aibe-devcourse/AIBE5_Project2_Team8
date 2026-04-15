c13d62fcc492e68b4e30975f03f6f0df9f8ff426
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2026-04-15 15:29:56 +0900

    fix: @EnableJpaAuditing 어노테이션 추가로 JPA Auditing 활성화

--- src/main/java/com/knoc/KnocApplication.java
+++ src/main/java/com/knoc/KnocApplication.java
@@ -2,8 +2,10 @@ package com.knoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KnocApplication {
 
 	public static void main(String[] args) {
