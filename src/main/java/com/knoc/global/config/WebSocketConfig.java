3825dbd94643806e8658873969851b80a72277af
devken65 <effortcoders@gmail.com>
2026-04-20 12:37:10 +0900

    feat: Setting WebSocket, Add Schedular

--- src/main/java/com/knoc/global/config/WebSocketConfig.java
+++ src/main/java/com/knoc/global/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.knoc.global.config;
 
+import com.knoc.auth.jwt.JwtHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
+    public WebSocketConfig(JwtHandshakeInterceptor jwtHandshakeInterceptor) {
+        this.jwtHandshakeInterceptor = jwtHandshakeInterceptor;
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -26,6 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // 연결 진입점
         registry.addEndpoint("/ws")
+                .addInterceptors(jwtHandshakeInterceptor)
                 .withSockJS(); // SockJS 폴백
     }
 }
