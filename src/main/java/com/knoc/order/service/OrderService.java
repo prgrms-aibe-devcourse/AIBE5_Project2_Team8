4e9ba4f17c3532a0d60ff3ea938aff7c57c3e093
SeheeKim <rlatpgml8135@naver.com>
2026-04-21 14:48:35 +0900

    refactor: 필요없는 import문 제거

--- src/main/java/com/knoc/order/service/OrderService.java
+++ src/main/java/com/knoc/order/service/OrderService.java
@@ -18,7 +18,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
