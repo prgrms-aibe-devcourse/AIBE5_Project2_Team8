35fb45e3f8dbaf7ae9ff7782a370dcdca50ca574
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2026-04-23 10:17:15 +0900

    feat: 생성자 어노테이션 추가

--- src/main/java/com/knoc/chat/dto/ChatMessageRequest.java
+++ src/main/java/com/knoc/chat/dto/ChatMessageRequest.java
@@ -1,8 +1,12 @@
 package com.knoc.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessageRequest {
     // JWT 인증완료 -> 내용만 보내고 Principal로 검증
 
