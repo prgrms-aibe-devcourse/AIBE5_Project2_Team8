bbafbfe7446ddd3273331e9bf9f3890a215b609e
SeheeKim <rlatpgml8135@naver.com>
2026-04-26 18:09:04 +0900

    feat: 리뷰 리포트 관련 에러코드 추가 후 적용 (#124)

--- src/main/java/com/knoc/workspace/WorkspaceFacadeService.java
+++ src/main/java/com/knoc/workspace/WorkspaceFacadeService.java
@@ -151,10 +151,10 @@ public class WorkspaceFacadeService {
         }
 
         ReviewRequest reviewRequest = reviewRequestRepository.findByOrder(order)
-                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
+                .orElseThrow(() -> new BusinessException(ErrorCode.REVIEW_REQUEST_REQUIRED_FOR_REPORT));
 
         if (reviewReportRepository.findByReviewRequest(reviewRequest).isPresent()) {
-            throw new BusinessException(ErrorCode.REVIEW_ALREADY_EXISTS);
+            throw new BusinessException(ErrorCode.REVIEW_REPORT_ALREADY_EXISTS);
         }
 
         reviewReportRepository.save(ReviewReport.builder()
@@ -191,10 +191,10 @@ public class WorkspaceFacadeService {
         }
 
         ReviewRequest reviewRequest = reviewRequestRepository.findByOrder(order)
-                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
+                .orElseThrow(() -> new BusinessException(ErrorCode.REVIEW_REQUEST_REQUIRED_FOR_REPORT));
 
         ReviewReport report = reviewReportRepository.findByReviewRequest(reviewRequest)
-                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
+                .orElseThrow(() -> new BusinessException(ErrorCode.REVIEW_REPORT_NOT_FOUND));
 
         report.update(industryPerspective, edgeCases, alternatives);
     }
